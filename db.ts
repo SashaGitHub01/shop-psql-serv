53a9e60345b78956d39c4b9cd228688652baf34a
SashaGitHub01 <sashab4b1n@yandex.ru>
2021-12-31 14:43:12 +0300

    fix env

--- db.ts
+++ db.ts
@@ -1,7 +1,10 @@
 import { Sequelize } from "sequelize";
+import dotenv from 'dotenv';
+dotenv.config();
 
-export const sequelize = new Sequelize('shop_db', 'postgres', '123456', {
-   host: 'localhost',
-   port: 5432,
+//@ts-ignore
+export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+   host: process.env.DB_HOST,
+   port: process.env.DB_PORT as unknown as number,
    dialect: 'postgres'
 })
